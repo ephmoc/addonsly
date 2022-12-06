ec6fb6342fc020162eacaf089ae0b1422830c834
Krzysztof Hubaczek <krzysztof.hubaczek@intive.com>
2022-12-06 15:02:58 +0100

    updated src_test file

--- test/src/src_test.dart
+++ test/src/src_test.dart
@@ -1,7 +1,9 @@
 import 'extensions/extensions_test.dart' as extensions;
+import 'widgets/row_with_columns_test.dart' as row_with_columns;
 import 'widgets/spacers_test.dart' as spacers;
 
 void main() {
   extensions.main();
   spacers.main();
+  row_with_columns.main();
 }
